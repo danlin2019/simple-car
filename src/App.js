90a5f8ab876b89fcff7b6de3e7f7a98925332688
danlin <asmob2011@gmail.com>
2024-10-21 17:57:29 +0800

    feat:car finish

--- src/App.js
+++ src/App.js
@@ -5,8 +5,44 @@ import Cart from "./components/Cart";
 import { CarContext } from "./store";
 function App() {
   const cartReducer = useReducer((state,action)=>{
+    const carList = [...state.carList]
+    console.log('carList',carList)
+    //先取得當前購物車目標的索引
+    const  index = carList.findIndex((item)=>item.id ===action.payload.id)
+    console.log('index',index,carList[index])
     console.log(action)
-    switch (action) {
+    switch (action.type) {
+      case 'ADD_TO_CARD':
+        if(index === -1 ) {
+          // 尚未加到購物車
+          carList.push(action.payload)
+        }else{
+          // 當前購物車的項目與加入項目一樣的時候
+          carList[index].quantity += action.payload.quantity
+        }            
+        return {
+          ...state,
+          carList,
+          // 先將金額 * 品項數量 再去做累加
+          total:calculateTotalPride(carList)
+        }
+      case 'CHANGE_CAR_QUANTITY':
+        carList[index].quantity = action.payload.quantity
+        return {
+          ...state,
+          carList,
+          // 先將金額 * 品項數量 再去做累加
+          total:calculateTotalPride(carList)
+        }
+
+      case 'REMOVE_CAR' : 
+        carList.splice(index,1)
+        return {
+          ...state,
+          carList,
+          // 先將金額 * 品項數量 再去做累加
+          total:calculateTotalPride(carList)
+        }
       default:
         return state
     }
@@ -29,6 +65,11 @@ function App() {
 
     </CarContext.Provider>
   );
+
+  function calculateTotalPride(carList) {
+    return carList.map((item) => item.quantity * item.price)
+      .reduce((a, b) => a + b, 0);
+  }
 }
 
 export default App;
